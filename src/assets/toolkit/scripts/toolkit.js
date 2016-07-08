95b43b86495996a51b4cc42799807d4b2e978d7b
Lucas Lemonnier <ll@ttwebco.com>
2016-07-08 13:51:39 -0400

    added markup, basic styles, and js for slideout nav

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,3 +1,21 @@
-/**
- * Toolkit JavaScript
- */
+// Slideout Navigation
+document.getElementById('nav-trigger').addEventListener('click', checkNav);
+window.addEventListener("keyup", function(e) {
+  if (e.keyCode == 27) closeNav();
+}, false);
+
+function checkNav() {
+  if (document.body.classList.contains('nav-container-active')) {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
+function closeNav() {
+  document.body.classList.remove('nav-container-active');
+}
+
+function openNav() {
+  document.body.classList.add('nav-container-active');
+}
