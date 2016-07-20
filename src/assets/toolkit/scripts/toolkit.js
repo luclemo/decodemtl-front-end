d63f01c3d154d363ab791442f161ef27b0610f54
Lucas Lemonnier <ll@ttwebco.com>
2016-07-20 14:47:02 -0400

    added faq list

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,21 +1,54 @@
+// ================================
 // Slideout Navigation
-document.getElementById('nav-trigger').addEventListener('click', checkNav);
-window.addEventListener("keyup", function(e) {
-  if (e.keyCode == 27) closeNav();
-}, false);
+// ================================
 
-function checkNav() {
-  if (document.body.classList.contains('nav-container-active')) {
-    closeNav();
-  } else {
-    openNav();
-  }
+if (document.getElementById("nav-trigger")) {
+	
+	document.getElementById('nav-trigger').addEventListener('click', checkNav);
+	window.addEventListener("keyup", function(e) {
+	  if (e.keyCode == 27) closeNav();
+	}, false);
+
+	function checkNav() {
+	  if (document.body.classList.contains('nav-container-active')) {
+	    closeNav();
+	  } else {
+	    openNav();
+	  }
+	}
+
+	function closeNav() {
+	  document.body.classList.remove('nav-container-active');
+	}
+
+	function openNav() {
+	  document.body.classList.add('nav-container-active');
+	}
 }
 
-function closeNav() {
-  document.body.classList.remove('nav-container-active');
-}
+// ================================
+// Accordion list (faq)
+// ================================
+var action = 'click';
+var speed = 300;
 
-function openNav() {
-  document.body.classList.add('nav-container-active');
-}
+$(document).ready( function() {
+    
+    // 	question handler
+   $('.question').on(action, function() {
+       
+       // get next element
+       $(this).next()
+           .slideToggle(speed)
+           		.siblings('.answer').slideUp(speed);
+       
+       // rotate arrow
+       var icon = $(this).children('.petal-icon');
+       
+       // remove rotate class
+       $('.petal-icon').not(icon).removeClass('rotate');
+       
+       // toggle rotate class
+       icon.toggleClass('rotate');
+   });
+});
